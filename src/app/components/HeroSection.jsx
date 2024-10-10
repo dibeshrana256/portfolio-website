40fcdb9028c6f24d093b0abc43f290a26e0c1dec
dibeshrana <dibeshrana.dr@gmail.com>
2024-10-10 13:23:12 -0500

    no message

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -32,7 +32,7 @@ export const HeroSection = () => {
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            voluptuous...
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
