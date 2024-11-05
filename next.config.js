fe9a2a7726d19869a329f0d3d80eecc85d5f88e2
dibeshrana <dibeshrana.dr@gmail.com>
2024-11-05 14:12:26 -0600

    configured the package.json to clear casche before deployment

--- next.config.js
+++ next.config.js
@@ -1,19 +1,41 @@
 // next.config.js
 
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   reactStrictMode: false,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: process.env.API_URL,
-  //     },
-  //   ],
-  // },
   httpAgentOptions: {
     keepAlive: true,
+  },
+  compress: true, // Enables gzip compression
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups.default = false;
+    }
+    config.performance = {
+      maxAssetSize: 25000000, // Set a 25MB limit for assets
+    };
+    return config;
   },
 };
 
 module.exports = nextConfig;
+
+// // next.config.js
+
+// const nextConfig = {
+//   output: 'standalone',
+//   reactStrictMode: false,
+//   // images: {
+//   //   remotePatterns: [
+//   //     {
+//   //       protocol: 'https',
+//   //       hostname: process.env.API_URL,
+//   //     },
+//   //   ],
+//   // },
+//   httpAgentOptions: {
+//     keepAlive: true,
+//   },
+// };
+
+// module.exports = nextConfig;
