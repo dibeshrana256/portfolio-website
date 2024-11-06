ee920c0c042cc91e2c37a90e97db82488fd84fba
dibeshrana <dibeshrana.dr@gmail.com>
2024-11-06 10:21:42 -0600

    correction to the .eslintcr.json file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // next.config.js
 
+// next.config.js
+
 const nextConfig = {
   output: "standalone",
   reactStrictMode: false,
@@ -7,13 +9,39 @@ const nextConfig = {
     keepAlive: true,
   },
   compress: true, // Enables gzip compression
+  productionBrowserSourceMaps: false, // Disable source maps in production
+
   webpack: (config, { isServer }) => {
+    // Client-side specific configuration
     if (!isServer) {
       config.optimization.splitChunks.cacheGroups.default = false;
     }
+
+    // Server-side specific configuration
+    if (isServer) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `npm.${packageName.replace("@", "")}`;
+            },
+          },
+        },
+      };
+    }
+
+    // Set a 25MB limit for assets
     config.performance = {
-      maxAssetSize: 25000000, // Set a 25MB limit for assets
+      maxAssetSize: 25000000,
     };
+
     return config;
   },
 };
